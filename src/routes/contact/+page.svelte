4d17f17967a1134a434643f191c4f539dd059f34
wasita <wasita.mahaphanit.gr@dartmouth.edu>
2024-03-03 22:55:33 -0500

    :lipstick: (contact)

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,13 +1,13 @@
-<h1 class="pt-4 pb-8 font-bold">contact</h1>
+<h1 class="pt-4 pb-8 font-bold">Contact</h1>
 <div class="container p-16 mx-auto my-4 rounded-xl">
 	<form action="https://formspree.io/f/xvoeogae" method="POST" class="space-y-1">
-		<label class="label" for="name">name</label>
+		<label class="label" for="name">Name</label>
 		<input class="input" type="text" name="name" id="name" placeholder="your name" required />
-		<label class="label" for="email">email</label>
+		<label class="label" for="email">E-mail</label>
 		<input class="input" type="email" name="_replyto" id="email" placeholder="youremail@provider.com" required/>
-		<label class="label" for="subject">subject</label>
+		<label class="label" for="subject">Subject</label>
 		<input class="input" type="text" name="subject" id="subject" placeholder="subject" />
-		<label class="label" for="message">message</label>
+		<label class="label" for="message">Message</label>
 		<textarea
 			class="input"
 			name="message"
@@ -15,6 +15,6 @@
 			id="message"
 			placeholder="write your message here..." 
 			required />
-		<button class="btn variant-glass-primary" type="submit">send</button>
+		<button class="btn variant-glass-primary" type="submit">Send</button>
 	</form>
 </div>
